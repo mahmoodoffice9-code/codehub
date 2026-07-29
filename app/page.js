edea1573f217e28998ee3f0202d3a517eef4f3aa
mahmoodoffice9-code <mahmoodoffice9@gmail.com>
2026-07-29 21:42:13 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -2,9 +2,10 @@
 import { useState, useEffect } from 'react'
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+)
 
 export default function Home() {
   const [assets, setAssets] = useState([])
